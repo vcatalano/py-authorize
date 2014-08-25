1fe5c57dd0d66c2fe258adf76486a255163fe197
Vincent Catalano <vincent@vincentcatalano.com>
2014-08-24 20:37:14 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Py-Authorize',
-    version='1.2.2.0',
+    version='1.2.2.1',
     author='Vincent Catalano',
     author_email='vincent@vincentcatlano.com',
     url='https://github.com/vcatalano/py-authorize',
