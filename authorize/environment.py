39247782694970946ee51e6dbdc2b42efc24ce2a
Eli Gundry <eligundry@gmail.com>
2016-04-18 21:06:59 -0400

    Switches PRODUCTION URL to Akamai URL

--- authorize/environment.py
+++ authorize/environment.py
@@ -2,4 +2,4 @@
 
 class Environment(object):
     TEST = 'https://apitest.authorize.net/xml/v1/request.api'
-    PRODUCTION = 'https://api.authorize.net/xml/v1/request.api'
+    PRODUCTION = 'https://api2.authorize.net/xml/v1/request.api'
